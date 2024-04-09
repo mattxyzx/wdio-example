d4c8c1eab1226f00df20cab5b1cade6c0c6e0930
mattxyzx <166439818+mattxyzx@users.noreply.github.com>
2024-04-09 21:45:29 +0200

    test commit

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,9 +1,6 @@
 import { browser } from '@wdio/globals'
 
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
+
 export default class Page {
     /**
     * Opens a sub page of the page
