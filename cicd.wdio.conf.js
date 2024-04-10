c84fa80482e0223180e293db7e94d3adc4fcb6a3
mattxyzx <166439818+mattxyzx@users.noreply.github.com>
2024-04-10 19:55:32 +0200

    test without capabilities

--- cicd.wdio.conf.js
+++ cicd.wdio.conf.js
@@ -1,5 +1,5 @@
 import { deepmerge } from 'deepmerge-ts';
-import wdioConfig from './wdio.conf.js';
+import wdioConfig = require('./wdio.conf.js');
 export const config = deepmerge(wdioConfig.config, {
     capabilities: [{
         browserName: 'chrome',
