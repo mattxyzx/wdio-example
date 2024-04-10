9eb288887d39d19d8fdd288c9cf000340f228fa9
mattxyzx <166439818+mattxyzx@users.noreply.github.com>
2024-04-10 19:48:19 +0200

    testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,7 +50,8 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {args: ['--no-sandbox', '--disable-dev-shm-usage', '--headless', '--disable-gpu',  '--remote-debugging-pipe']}
     }],
 
     //
