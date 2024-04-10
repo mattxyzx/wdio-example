0973fd71e306172984a5587772a406d113da3058
mattxyzx <166439818+mattxyzx@users.noreply.github.com>
2024-04-10 18:24:25 +0200

    adds configs

---
+++ local.wdio.conf..js
@@ -0,0 +1,8 @@
+import { deepmerge } from 'deepmerge-ts';
+import wdioConfig from './wdio.conf.js';
+export const config = deepmerge(wdioConfig.config, {
+    capabilities: [{
+        browserName: 'chrome',
+    }],
+    specs: ['specs/*']
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -100,7 +100,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['devtools', 'vscode', 'eslinter', 'docker'],
+    services: ['devtools', 'vscode', 'docker'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -123,7 +123,7 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','timeline'],
+    reporters: ['spec', ['timeline', { outputDir: './logs' }]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
